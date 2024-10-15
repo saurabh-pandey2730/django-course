1d2a52c417feb45b7cd33525c68d0e39f633622b
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-10-15 21:22:45 +0530

    adding more dynamic route

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -3,14 +3,30 @@ from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 
+monthly_challenges = {
+       "january":"hello january",
+       "february" :"hello february",
+       "march" :"hello march",
+       "april" :"hello april",
+       "may" :"hello may",
+       "june" :"hello june",
+       "july" :"hello july",
+       "august" :"hello august",
+       "september" :"hello september",
+       "october" :"hello october",
+       "november" :"hello november",
+       "december" :"hello december",
+    
+}
+
+def monthly_challenge_by_number(request,month):
+     return HttpResponse(month)
 
 def monthly_challenge(request,month):
-    challenge_text =None
-    
-    if month == 'january':
-        challenge_text = "january month !"
-    elif month == 'february':
-        challenge_text = "february month !"
-    else :
-        return HttpResponseNotFound("unknown month")
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text =monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("month not found")
+        
+
