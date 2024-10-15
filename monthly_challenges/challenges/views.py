1f05b629af25c307b537e1455e21d31d268d5fa0
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-10-15 20:23:26 +0530

    multiple views completed

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse('this works!')
+def january(request):
+    return HttpResponse('this works january!')
+
+def february(request):
+    return HttpResponse('this works! february')
