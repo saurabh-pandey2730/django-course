ff9d738ee4403617a550e9fb5a1b014776be3f09
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-10-15 20:30:39 +0530

    dynamic path segment

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
-def january(request):
-    return HttpResponse('this works january!')
 
-def february(request):
-    return HttpResponse('this works! february')
+
+def monthly_challenge(request,month):
+    challenge_text =None
+    
+    if month == 'january':
+        challenge_text = "january month !"
+    elif month == 'february':
+        challenge_text = "february month !"
+    else :
+        return HttpResponseNotFound("unknown month")
+    return HttpResponse(challenge_text)
